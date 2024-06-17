03f6cc6b5e1c4ed3248fbd922e3337f350f27c2b
waitorgr <github0505@gmail.com>
2024-06-17 19:12:27 +0300

    Added the ability to view the item. Added similar items in the item view

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Item
 
-# Create your views here.
+
+def detail(request, pk):
+    item = get_object_or_404(Item, pk=pk) #пошук itema
+    related_items=Item.objects.filter(Category=item.Category, is_sold=False).exclude(pk=pk)[0:3] #пошук предмета в одній категорії
+
+    return render(request, 'item/detail.html',{
+        'item': item,
+        'related_items': related_items
+    })
